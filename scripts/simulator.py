e29e2b6c9c0d6643036e9dd0552dba688933db5b
Tomev <rybotycki.tomasz+github@gmail.com>
2026-07-24 23:55:21 +0200

    feat(sim): Add readout errors extraction methods.

--- scripts/simulator.py
+++ scripts/simulator.py
@@ -3,6 +3,7 @@ A module containing functionalities for IQM `siruis` simulator creation. It's ac
 """
 
 import os
+from collections import defaultdict
 from typing import Literal
 
 from iqm.iqm_client import IQMClient, StaticQuantumArchitecture
@@ -77,7 +78,7 @@ def get_error_profile(system_name: str) -> IQMErrorProfile:
         two_qubit_gate_depolarizing_error_parameters={},
         single_qubit_gate_durations={},
         two_qubit_gate_durations={},
-        readout_errors={},
+        readout_errors=get_readout_errors(quality_metric_set),
     )
 
 
@@ -93,6 +94,24 @@ def get_ts(
             ts[component_name] = observation.value * 1e9  # seconds to nano seconds
 
     return ts
+
+
+def get_readout_errors(
+    quality_metric_set: ObservationSetWithObservations,
+) -> dict[str, dict[str, float]]:
+    """TODO(TR): Docstring"""
+    readout_errors: dict[str, dict[str, float]] = defaultdict(lambda: {})
+
+    for observation in quality_metric_set.observations:
+        if "error_0_to_1" in observation.dut_field:
+            component_name: str = observation.dut_field.split(".")[-2]
+            readout_errors[component_name]["0"] = observation.value
+
+        if "error_1_to_0" in observation.dut_field:
+            component_name: str = observation.dut_field.split(".")[-2]
+            readout_errors[component_name]["1"] = observation.value
+
+    return readout_errors
 
 
 def FakeFromBackend(system_name: str) -> IQMFakeBackend:
@@ -117,17 +136,37 @@ def main() -> None:
         quantum_computer=os.environ["IQM_COMPUTER"],  # sirius
     )
 
+    provider: IQMProvider = IQMProvider(
+        os.environ["IQM_PROVIDER"],
+        quantum_computer=os.environ["IQM_COMPUTER"],
+    )
+
+    backend: IQMBackend = provider.get_backend()
     calibration_set: ObservationSetWithObservations = client.get_calibration_set()
     quality_metric_set: ObservationSetWithObservations = client.get_quality_metric_set(
         calibration_set.observation_set_id
     )
 
-    for x in quality_metric_set.observations:
-        if "t1" in x.dut_field:
-            print(f"\n{x}\n")
+    print(get_readout_errors(quality_metric_set))
+    return
 
-    print(get_ts(quality_metric_set, "t1"))
-    print(get_ts(quality_metric_set, "t2"))
+    # for k, v in backend.architecture.gates.items():
+    #    print(f"\n{k}: {v}")
+
+    value_sought: str = "error_1_to_0"
+
+    """
+    for observation in calibration_set.observations:
+        if value_sought in observation.dut_field:
+            print(f"\n{observation}\n")
+    """
+
+    for observation in quality_metric_set.observations:
+        if value_sought in observation.dut_field:
+            print(f"\n{observation}\n")
+
+    # print(get_ts(quality_metric_set, "t1"))
+    # print(get_ts(quality_metric_set, "t2"))
 
 
 if __name__ == "__main__":
