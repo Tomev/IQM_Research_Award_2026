74051847e9eb953aeb81748ced4bc656e90d1abb
Tomev <rybotycki.tomasz+github@gmail.com>
2026-07-25 01:27:43 +0200

    feat(sim): Add initial version of sirius simulator.

--- scripts/simulator.py
+++ scripts/simulator.py
@@ -13,14 +13,23 @@ from iqm.qiskit_iqm.fake_backends.iqm_fake_backend import (
     IQMFakeBackend,
 )
 from iqm.station_control.interface.models import ObservationSetWithObservations
+from numpy import average
 
 TimeType = Literal["t1", "t2"]
 
-from numpy import average
-
 
 def get_architecture(system_name: str) -> StaticQuantumArchitecture:
-    """TODO(TR): Docstring"""
+    """Returns the static quantum architecture for the given system.
+
+    This function constructs a :class:`StaticQuantumArchitecture` object based on the
+    specified system name, using the backend information retrieved from the IQM provider.
+
+    Args:
+        system_name: The name of the quantum system for which the architecture is to be retrieved.
+
+    Returns:
+        StaticQuantumArchitecture: The static quantum architecture for the given system.
+    """
 
     provider: IQMProvider = IQMProvider(
         os.environ["IQM_PROVIDER"],
@@ -85,22 +94,34 @@ def get_error_profile(system_name: str) -> IQMErrorProfile:
     return IQMErrorProfile(
         t1s=get_ts(quality_metric_set, "t1"),
         t2s=get_ts(quality_metric_set, "t2"),
-        single_qubit_gate_depolarizing_error_parameters={},
-        two_qubit_gate_depolarizing_error_parameters={},
-        single_qubit_gate_durations=get_gates_duration(
-            calibration_set, backend_gates["1q"]
+        single_qubit_gate_depolarizing_error_parameters=compute_single_qubit_gates_depolarizing_error_parameters(
+            quality_metric_set, backend_gates["1q"]
         ),
-        two_qubit_gate_durations=get_gates_duration(
-            calibration_set, backend_gates["2q"]
+        two_qubit_gate_depolarizing_error_parameters=compute_two_qubit_gates_depolarizing_error_parameters(
+            quality_metric_set, backend_gates["2q"]
         ),
+        single_qubit_gate_durations=get_gates_duration(calibration_set, backend_gates["1q"]),
+        two_qubit_gate_durations=get_gates_duration(calibration_set, backend_gates["2q"]),
         readout_errors=get_readout_errors(quality_metric_set),
     )
 
 
-def get_ts(
-    quality_metric_set: ObservationSetWithObservations, time_type: TimeType
-) -> dict[str, float]:
-    """TODO(TR): Docstring"""
+def get_ts(quality_metric_set: ObservationSetWithObservations, time_type: TimeType) -> dict[str, float]:
+    """Returns the T1 or T2 times for each component (qubit / resonator) from the quality metric set.
+
+    This function processes the observations in the quality metric set and extracts the T1 or T2 times based on the
+    specified time type. It maps each observation to a component name and converts the values from seconds to
+    nanoseconds, as expected by the `IQMErrorProfile`.
+
+    Args:
+        quality_metric_set:
+            The set of observations containing quality metrics.
+        time_type:
+            The type of time to extract, either "t1" or "t2".
+
+    Returns:
+        dict[str, float]: A dictionary mapping component names to their corresponding T1 or T2 times in nanoseconds.
+    """
     ts: dict[str, float] = {}
 
     for observation in quality_metric_set.observations:
@@ -126,9 +147,66 @@ def get_gates(backend: IQMBackend) -> dict[str, list[str]]:
     return backend_gates
 
 
-def get_gates_duration(
-    calibration_set: ObservationSetWithObservations, gates: list[str]
-) -> dict[str, float]:
+def compute_single_qubit_gates_depolarizing_error_parameters(
+    quality_metric_set: ObservationSetWithObservations, gates: list[str]
+) -> dict[str, dict[str, float]]:
+    """TODO(TR): Docstring
+
+    .. note::
+        Depolarizing error parameter can be obtained from the fidelity. In the case of 1-qubit gates it's given by
+
+        .. math::
+
+            p_{depolarization}(F) = 2 * (1 - F),
+
+        where :math:`F` is the fidelity.
+    """
+    depolarizing_error_parameters: dict[str, dict[str, float]] = defaultdict(lambda: {})
+
+    for observation in quality_metric_set.observations:
+        if ".fidelity" in observation.dut_field:
+            gate_name: str = observation.dut_field.split(".")[2]
+
+            if gate_name not in gates:  # Omit the gates that are of no interest to us.
+                continue
+
+            component_name: str = observation.dut_field.split(".")[-2]
+            depolarizing_error_parameters[gate_name][component_name] = 2 * (1 - observation.value)
+
+    return depolarizing_error_parameters
+
+
+def compute_two_qubit_gates_depolarizing_error_parameters(
+    quality_metric_set: ObservationSetWithObservations, gates: list[str]
+) -> dict[str, dict[tuple[str, str], float]]:
+    """TODO(TR): Docstring
+
+    .. note::
+        Depolarizing error parameter can be obtained from the fidelity. In the case of 2-qubit gates it's given by
+
+        .. math::
+
+            p_{depolarization}(F) = 4/3 * (1 - F),
+
+        where :math:`F` is the fidelity.
+    """
+    depolarizing_error_parameters: dict[str, dict[tuple[str, str], float]] = defaultdict(lambda: {})
+
+    for observation in quality_metric_set.observations:
+        if ".fidelity" in observation.dut_field:
+            gate_name: str = observation.dut_field.split(".")[2]
+
+            if gate_name not in gates:  # Omit the gates that are of no interest to us.
+                continue
+
+            operation_components: list[str] = observation.dut_field.split(".")[-2].split("__")
+            inner_key: tuple[str, str] = (operation_components[0], operation_components[1])
+            depolarizing_error_parameters[gate_name][inner_key] = 4 / 3 * (1 - observation.value)
+
+    return depolarizing_error_parameters
+
+
+def get_gates_duration(calibration_set: ObservationSetWithObservations, gates: list[str]) -> dict[str, float]:
     """TODO(TR): Docstring
 
     .. note::
@@ -149,9 +227,7 @@ def get_gates_duration(
 
             # print(f"\n{observation}\n")
 
-    return {
-        k: average(v) * 1e9 for k, v in gates_duration.items()
-    }  # convert seconds to nano seconds
+    return {k: average(v) * 1e9 for k, v in gates_duration.items()}  # convert seconds to nano seconds
 
 
 def get_readout_errors(
@@ -207,7 +283,17 @@ def main() -> None:
 
     gates: dict[str, list[str]] = get_gates(backend)
 
-    value_sought: str = "fidelity"
+    depolarizing_errors: dict[str, dict[str, float]] = compute_two_qubit_gates_depolarizing_error_parameters(
+        quality_metric_set, gates["2q"]
+    )
+
+    for k, v in depolarizing_errors.items():
+        print(f"\n{k}: {v}")
+
+    print()
+    return
+
+    value_sought: str = ".prx."
 
     print("\n\nCalibration:")
 
