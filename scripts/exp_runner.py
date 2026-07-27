1d39208e9084982df5c2639bec571284c2893518
Tomev <rybotycki.tomasz+github@gmail.com>
2026-07-28 01:06:00 +0200

    update(exp): Update exp runner to run noisy simulation by default (to avoid accidental credits loss).

--- scripts/exp_runner.py
+++ scripts/exp_runner.py
@@ -14,8 +14,8 @@ def main():
     qubits_lists: list[list[int] | tuple[int, ...]] = get_layouts_from_layouts_info(
         "data/noisy_1e4_shots/2026-07-27_203226_layouts_info.json"
     )
-    # noisy_pipeline(qubits_lists)
-    device_pipeline(qubits_lists)
+    noisy_pipeline(qubits_lists)
+    # device_pipeline(qubits_lists)
     print(f"{datetime.now()}: Done")
 
 
